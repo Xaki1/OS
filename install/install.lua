e3e5d8f0785642720a75eeb116cb16a746752bd7
Xaki1 <95175899+Xaki1@users.noreply.github.com>
2022-08-22 10:29:36 +0500

    Update install.lua

--- install/install.lua
+++ install/install.lua
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/Xaki1/OS/main/bin/address.lua
 wget https://raw.githubusercontent.com/Xaki1/OS/main/bin/alias.lua
 wget https://raw.githubusercontent.com/Xaki1/OS/main/bin/cat.lua
-wget https://github.com/Xaki1/OS/tree/main/bin/cd.lua
+wget https://raw.githubusercontent.com/Xaki1/OS/tree/main/bin/cd.lua
